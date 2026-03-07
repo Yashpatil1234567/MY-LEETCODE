713d21df7c537a96655e74da6c1b1a1bad0eac69
Yash <yp421416@gmail.com>
2026-03-07 23:59:03 +0530

    Add expect function with toBe and notToBe methods

---
+++ To Be or Not To Be .js
@@ -0,0 +1,24 @@
+var expect = function(val) {
+    
+    return {
+        
+        toBe: function(val2) {
+            if(val === val2){
+                return true;
+            }
+            else{
+                throw new Error("Not Equal");
+            }
+        },
+
+        notToBe: function(val2) {
+            if(val !== val2){
+                return true;
+            }
+            else{
+                throw new Error("Equal");
+            }
+        }
+
+    };
+};
